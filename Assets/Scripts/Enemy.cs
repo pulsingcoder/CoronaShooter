1ddb33409217b771eef47d368d3f4eb39032c2bc
Yuvraj Darwani <44190710+pulsingcoder@users.noreply.github.com>
2020-03-26 21:41:41 +0530

    GunSetUp

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Health = 50f;
+    public void TakeDamage(float amount)
+    {
+        Health -= amount;
+        if (Health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    
+}
---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float impactForce = 30f;
+  //  [SerializeField] Joystick gunJoystick;
+    [SerializeField] Button buttonShoot;
+    // Update is called once per frame
+    void Update()
+    {
+       // buttonShoot.onClick.AddListener(TaskOnClick);
+     
+    }
+
+    private void TaskOnClick()
+    {
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        // shoot direction in we're facing fpscam.transform.forward
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy !=null)
+            {
+                enemy.TakeDamage(damage);
+
+            }
+            GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGo, 2f);
+            if (hit.rigidbody !=null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+        }
+    }
+}
