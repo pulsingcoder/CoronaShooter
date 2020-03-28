af93319f4c94b2e15893a1d02139d05a44129130
Yuvraj Darwani <44190710+pulsingcoder@users.noreply.github.com>
2020-03-28 20:52:53 +0530

    Sanitizing

---
+++ Assets/Scripts/WayPointPatrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WayPointPatrol : MonoBehaviour
+{
+    public Transform player;
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    int currentWayPointIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            currentWayPointIndex = (currentWayPointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[currentWayPointIndex].position);
+        }
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance < 3)
+        {
+            navMeshAgent.SetDestination(player.position);
+        }
+    }
+}
